9ae0120027e8717f33cacb097da23228214f9f68
chibakaBoss <chinbat.purevdorjj@gmail.com>
2025-08-12 23:17:23 +0800

    config add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // Зургийг Next.js Image Optimization-г хаах
+  },
 };
 
 export default nextConfig;
+
+
